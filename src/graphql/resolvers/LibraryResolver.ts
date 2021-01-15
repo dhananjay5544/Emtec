a8831003dc0b5a49418b4d48a23fb6cf48b07527
dhananjay5544 <dhananjayshinde1999@gmail.com>
2021-01-16 00:25:46 +0530

    library service merged

--- src/graphql/resolvers/LibraryResolver.ts
+++ src/graphql/resolvers/LibraryResolver.ts
@@ -1,14 +1,11 @@
 import { Resolver, Mutation, Arg, Query } from "type-graphql";
-import { getManager } from "typeorm";
 import { Library } from "../entity/Library";
-import { Book } from "../entity/Book";
+import libraryService from "../../services";
 import {
   ActionResponse,
   BookIssueInput,
   BookReturnInput,
 } from "../types/librarytypes";
-
-import { User } from "../entity/User";
 
 @Resolver()
 export class LibraryResolver {
@@ -17,51 +14,7 @@ export class LibraryResolver {
   async issueBook(
     @Arg("options", () => BookIssueInput) options: BookIssueInput
   ) {
-    // check user exists or not
-    const user = await User.find({ where: { user_id: options.userid } });
-
-    if (user.length !== 0) {
-      // check book already issued
-      const isIssued = await Library.find({
-        where: {
-          bookid: options.bookid,
-          userid: options.userid,
-          status: "issued",
-        },
-      });
-
-      if (isIssued.length === 0) {
-        await Library.create({ ...options, status: "issued" }).save();
-        const entityManager = getManager();
-        await entityManager.query(
-          `Insert into book_users_user values(${options.bookid},${options.userid})`
-        );
-        await entityManager.query(
-          `Insert into user_books_book values(${options.userid},${options.bookid})`
-        );
-        await entityManager.decrement(
-          Book,
-          { book_id: options.bookid },
-          "quantity",
-          1
-        );
-        return {
-          status: true,
-          msg: `book has been issued to user ${options.userid}`,
-          reminder: "reminder has been set",
-        };
-      } else {
-        return {
-          status: false,
-          msg: `Book has been already issued to ${user[0].firstname}`,
-        };
-      }
-    } else {
-      return {
-        status: false,
-        msg: `User not found`,
-      };
-    }
+    return await libraryService.issueBook(options);
   }
 
   // Mutation: return book
@@ -69,43 +22,7 @@ export class LibraryResolver {
   async returnBook(
     @Arg("options", () => BookReturnInput) options: BookReturnInput
   ) {
-    const exists = await Library.count({
-      where: {
-        bookid: options.bookid,
-        userId: options.userid,
-        status: "issued",
-      },
-    });
-    if (exists) {
-      await Library.update(
-        { bookid: options.bookid, userid: options.userid },
-        { status: "returned" }
-      );
-      const entityManager = getManager();
-      await entityManager.query(
-        `Delete from book_users_user where userUserId=${options.userid} and bookBookId=${options.bookid}`
-      );
-
-      await entityManager.query(
-        `Delete from user_books_book where userUserId=${options.userid} and bookBookId=${options.bookid}`
-      );
-
-      await entityManager.increment(
-        Book,
-        { book_id: options.bookid },
-        "quantity",
-        1
-      );
-      return {
-        status: true,
-        msg: `book has been returned by user ${options.userid}`,
-      };
-    } else {
-      return {
-        status: false,
-        msg: `book was not issued to user ${options.userid}`,
-      };
-    }
+    return await libraryService.returnBook(options);
   }
 
   // Query: library books history
