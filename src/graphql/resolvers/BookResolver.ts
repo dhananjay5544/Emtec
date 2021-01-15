321c5f7118078088866ffcae0a16ec522eb8aaed
dhananjay5544 <dhananjayshinde1999@gmail.com>
2021-01-16 00:25:19 +0530

    book service merged

--- src/graphql/resolvers/BookResolver.ts
+++ src/graphql/resolvers/BookResolver.ts
@@ -1,42 +1,37 @@
 import { Resolver, Mutation, Arg, Int, Query } from "type-graphql";
 import { Book } from "../entity/Book";
 import { BookInput, BookUpdateInput, BookOutput } from "../types/booktypes";
+import bookService from "../../services";
 
 @Resolver()
 export class BookResolver {
-  // Mutation: insert book
+  // Mutation: add new book
   @Mutation(() => Book)
   async createBook(@Arg("options", () => BookInput) options: BookInput) {
-    const book = await Book.create(options).save();
-    return book;
+    return await bookService.addBook(options);
   }
 
   // Mutation: update book
-  @Mutation(() => Boolean)
+  @Mutation(() => String)
   async updateBook(
     @Arg("id", () => Int) id: number,
     @Arg("input", () => BookUpdateInput) input: BookUpdateInput
   ) {
-    const book = await Book.findOne({ where: { book_id: id } });
-    if (!book) throw new Error("Book not found!");
-    Object.assign(book, input);
-    await book.save();
-    return true;
+    const res = await bookService.updateBook(id, input);
+    return res.msg;
   }
 
   // Mutation: delete book
-  @Mutation(() => Boolean)
+  @Mutation(() => String)
   async deleteBook(@Arg("id", () => Int) id: number) {
-    const book = await Book.findOne({ where: { book_id: id } });
-    if (!book) throw new Error("Book not found!");
-    await Book.delete({ book_id: id });
-    return true;
+    const res = await bookService.deleteBook(id);
+    return res.msg;
   }
 
   // Query: get all books
   @Query(() => [BookOutput])
   async Books() {
-    const books = await Book.find({ relations: ["users"] });
+    const books = await bookService.getBooks();
     return books;
   }
 }
